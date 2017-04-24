ce0cf60dcc81143f6dbe94feda1dceb8b5f34c56
hadi ranjbar <ranjbar.hadi@live.com>
2017-04-24 16:58:39 +0430

    fixing myket package names

--- app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
+++ app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
@@ -262,8 +262,8 @@ public class IabHelper {
             }
         };
 
-        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.android.vending");
+        Intent serviceIntent = new Intent("ir.mservices.market.InAppBillingService.BIND");
+        serviceIntent.setPackage("ir.mservices.market");
         if (!mContext.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
             // service available to handle that Intent
             mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
